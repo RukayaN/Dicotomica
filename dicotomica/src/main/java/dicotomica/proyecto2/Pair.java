731385b4dba1a8dfb8db65352ba6c8095b736ec7
Rukaya Nassereddine <na.rukaya@correo.unimet.edu.ve>
2025-03-20 21:12:10 -0400

    Clase para manejar pares clave-valor

---
+++ dicotomica/src/main/java/dicotomica/proyecto2/Pair.java
@@ -0,0 +1,11 @@
+package dicotomica.proyecto2;
+
+public class Pair {
+    public String key;
+    public boolean value;
+    
+    public Pair(String key, boolean value) {
+        this.key = key;
+        this.value = value;
+    }
+}
